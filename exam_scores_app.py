5444489815f73793b265cf1922e4ba21e2649445
anshikak1 <143382493+anshikak1@users.noreply.github.com>
2023-09-28 19:44:02 +0000

    exam_scores_app

--- exam_scores_app.py
+++ exam_scores_app.py
@@ -15,7 +15,7 @@ def create_bar_chart(data):
     st.header("Bar Chart of Top 10 Students")
     top_10_students = data.sort_values(by='Score', ascending=False).head(10)
     fig, ax = plt.subplots()
-    ax.bar(top_10_students['Student_Name'], top_10_students['Score'])
+    ax.bar(top_10_students['Student_Name'], top_10_students["Score"])
     ax.set_xlabel('Student Name')
     ax.set_ylabel('Exam Score')
     ax.set_xticklabels(top_10_students['Student_Name'], rotation=45)
